dbcdbefba3ea6cc70733a5fbd411e3d267711c0a
Nathan Ferig <nathan.ferig@gmail.com>
2021-06-04 20:45:33 -0300

    Add some  more tests

--- unittest_example.py
+++ unittest_example.py
@@ -8,6 +8,7 @@ import unittest
 
 from doctest_example import factorial
 from doctest_example import greatest_common_factor
+from doctest_example import least_common_multiple
 
 class TestFactorial(unittest.TestCase):
 
@@ -60,3 +61,33 @@ class TestGCF(unittest.TestCase):
             greatest_common_factor,1e100,10)
         self.assertRaises(OverflowError,
             greatest_common_factor,10,1e100)
+
+class TestLCM(unittest.TestCase):
+
+    def test_equal(self):
+        self.assertEqual(
+            least_common_multiple(3,5),15)
+        self.assertEqual(
+            least_common_multiple(2,8),8)
+        self.assertEqual(
+            least_common_multiple(8,12),24)
+        self.assertEqual(
+            least_common_multiple(12,8),24)
+
+    def test_negative(self):
+        self.assertRaises(ValueError,
+            least_common_multiple,-5,10)
+        self.assertRaises(ValueError,
+            least_common_multiple,5,-10)
+
+    def test_float(self):
+        self.assertRaises(ValueError,
+            least_common_multiple,5.5,10)
+        self.assertRaises(ValueError,
+            least_common_multiple,5,10.6)
+
+    def test_overflow(self):
+        self.assertRaises(OverflowError,
+            least_common_multiple,1e100,10)
+        self.assertRaises(OverflowError,
+            least_common_multiple,10,1e100)
