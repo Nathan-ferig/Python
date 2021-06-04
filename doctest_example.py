e66b67d313ce1675b898892dce663da6a6748e30
Nathan Ferig <nathan.ferig@gmail.com>
2021-06-04 18:31:10 -0300

    d5e9c11

--- doctest_example.py
+++ doctest_example.py
@@ -26,6 +26,8 @@ def least_common_multiple(a: int,b: int) -> int:
     24
     
     """
+    value_test(a)
+    value_test(b)
     c = greatest_common_factor(a,b)
     return int((a*b)/c)
 
@@ -44,6 +46,8 @@ def greatest_common_factor(a: int,b: int) -> int:
     >>> greatest_common_factor(7,5)
     1
     """
+    value_test(a)
+    value_test(b)
     n = 0
     if a > b:
         while b != 0:
@@ -58,4 +62,54 @@ def greatest_common_factor(a: int,b: int) -> int:
             a = n
         return int(b)
 
-print(least_common_multiple(3,5))
+def factorial(n: int = 5) -> int:
+    """This function calculates the factorial of n:
+
+    >>> factorial(5)
+    24
+
+    >>> factorial(1)
+    1
+
+    >>> factorial(0)
+    1
+
+    """
+    value_test(n)
+    if type(n) == float: n = int(n)
+    factorial = 1
+    for i in range(1,n):
+        factorial *= i
+    return factorial
+
+def value_test(n: int) -> None:
+    """This function runs validation tests for a given number n:
+
+    >>> value_test(5)
+    
+    >>> value_test(-5)
+    Traceback (most recent call last):
+        ...
+    ValueError: Value must be a positive integer.
+
+    >>> value_test(5.5)
+    Traceback (most recent call last):
+        ...
+    ValueError: Value must be an integer.
+
+    >>> value_test(1e100)
+    Traceback (most recent call last):
+        ...
+    OverflowError: Value is too large.
+
+    """
+    if n < 0:
+        raise ValueError('Value must be a positive integer.')
+    if int(n) != n:
+        raise ValueError('Value must be an integer.')
+    if n + 1 == n:
+        raise OverflowError('Value is too large.')
+
+# print(least_common_multiple(3,5))
+# print(greatest_common_factor(3,5))
+# print(factorial(5.0))
