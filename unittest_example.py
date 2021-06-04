d0923e056dc2d4825f7d15accc1d2a15b78eec7a
Nathan Ferig <nathan.ferig@gmail.com>
2021-06-04 20:42:57 -0300

    Add more tests

--- unittest_example.py
+++ unittest_example.py
@@ -7,30 +7,17 @@ python -m unittest -v unittest_example.py
 import unittest
 
 from doctest_example import factorial
+from doctest_example import greatest_common_factor
 
 class TestFactorial(unittest.TestCase):
 
     def test_positive(self):
         self.assertEqual(
-            factorial(5),
-            24
-        )
-
-    def test_positive(self):
+            factorial(5),24)
         self.assertEqual(
-            factorial(1),
-            1
-        )
-
-    def test_positive(self):
+            factorial(1),1)
         self.assertEqual(
-            factorial(0),
-            1
-        )
-
-    def test_negative(self):
-        self.assertRaises(ValueError,
-            factorial,-5)
+            factorial(0),1)
 
     def test_negative(self):
         self.assertRaises(ValueError,
@@ -43,3 +30,33 @@ class TestFactorial(unittest.TestCase):
     def test_overflow(self):
         self.assertRaises(OverflowError,
             factorial,1e100)
+
+class TestGCF(unittest.TestCase):
+
+    def test_equal(self):
+        self.assertEqual(
+            greatest_common_factor(15,35),5)
+        self.assertEqual(
+            greatest_common_factor(48,72),24)
+        self.assertEqual(
+            greatest_common_factor(35,15),5)
+        self.assertEqual(
+            greatest_common_factor(7,5),1)
+
+    def test_negative(self):
+        self.assertRaises(ValueError,
+            greatest_common_factor,-5,10)
+        self.assertRaises(ValueError,
+            greatest_common_factor,5,-10)
+
+    def test_float(self):
+        self.assertRaises(ValueError,
+            greatest_common_factor,5.5,10)
+        self.assertRaises(ValueError,
+            greatest_common_factor,5,10.6)
+
+    def test_overflow(self):
+        self.assertRaises(OverflowError,
+            greatest_common_factor,1e100,10)
+        self.assertRaises(OverflowError,
+            greatest_common_factor,10,1e100)
